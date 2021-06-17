179b9f6c5030228eeffa42084dd9687b4058fb90
aniruddha.sanyal <aniruddha.sanyal@aryaka.com>
2021-06-17 19:19:00 +0530

    2

--- src/App.vue
+++ src/App.vue
@@ -8,8 +8,8 @@
 </template>
 
 <script>
-import NavBar  from './components/NavBar.vue';
-import Footer  from './components/Footer.vue';
+import NavBar  from '@/components/NavBar.vue';
+import Footer  from '@/components/Footer.vue';
 
 export default {
   components: { 
